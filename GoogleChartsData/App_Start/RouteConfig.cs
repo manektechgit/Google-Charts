6b887a266d18cb5834996839ddb66bb41e214f3d
ManekTech <98881207+manektechgit@users.noreply.github.com>
2022-02-04 19:16:50 +0530

    Update RouteConfig.cs

--- GoogleChartsData/App_Start/RouteConfig.cs
+++ GoogleChartsData/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace GoogleChartsData
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "GoogleChart", action = "GoogleCharts", id = UrlParameter.Optional }
             );
         }
     }
